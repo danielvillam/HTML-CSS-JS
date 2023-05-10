5d8027b453c65d582685bd72384386b9c16599e0
danielvillam <jvillame@unal.edu.co>
2023-05-10 16:32:57 -0500

    Add code.js

---
+++ Convert to Grayscale/js/code.js
@@ -0,0 +1,19 @@
+var image;
+
+function upload(){
+    var imgcanvas = document.getElementById("canvas");
+    var fileinput = document.getElementById("finput");
+    image = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas);
+}
+
+function makeGray(){
+    for(var pixel of img.values()){
+        var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgcanvas = document.getElementById("canvas");
+    image.drawTo(imgcanvas);
+}
