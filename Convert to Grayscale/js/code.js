2ce289be1392eb0117263b6a8f232d91831a8b72
danielvillam <jvillame@unal.edu.co>
2023-05-10 16:51:52 -0500

    Finished page

--- Convert to Grayscale/js/code.js
+++ Convert to Grayscale/js/code.js
@@ -1,19 +1,21 @@
 var image;
 
+// upload image to canvas1
 function upload(){
-    var imgcanvas = document.getElementById("canvas");
+    var imgcanvas = document.getElementById("canvas1");
     var fileinput = document.getElementById("finput");
     image = new SimpleImage(fileinput);
     image.drawTo(imgcanvas);
 }
 
+// draw grayscale image on canvas2
 function makeGray(){
-    for(var pixel of img.values()){
+    for(var pixel of image.values()){
         var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
         pixel.setRed(avg);
         pixel.setGreen(avg);
         pixel.setBlue(avg);
     }
-    var imgcanvas = document.getElementById("canvas");
+    var imgcanvas = document.getElementById("canvas2");
     image.drawTo(imgcanvas);
 }
