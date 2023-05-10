72365519697fdfcdbab7cc9bd462a42fb92a8f60
danielvillam <jvillame@unal.edu.co>
2023-05-10 16:09:24 -0500

    Finished page

--- Upload Image Prototype/js/code.js
+++
@@ -1,7 +0,0 @@
-function upload(){
-    // get input from text input
-    var fileinput = document.getElementById("finput");
-    var filename = fileinput.value;
-    // alert displaying text
-    alert("You chose " + filename);
-}
