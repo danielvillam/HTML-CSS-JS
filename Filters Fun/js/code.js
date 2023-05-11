485cb2acacb80a62c3bf7dfe67b5ccabc59f2eda
danielvillam <jvillame@unal.edu.co>
2023-05-11 15:23:44 -0500

    Filter gray to Filters Fun

--- Filters Fun/js/code.js
+++ Filters Fun/js/code.js
@@ -1,23 +1,42 @@
-var image;
-var imageDefault;
+var grayImage;
+var redImage;
+var image = null;
+var canvas = null;
 
 // upload image to canvas1
 function upload(){
-    var imgcanvas = document.getElementById("can");
+    canvas = document.getElementById("can");
     var fileinput = document.getElementById("finput");
-    imageDefault = new SimpleImage(fileinput);
-    imageDefault.drawTo(imgcanvas);
+    image = new SimpleImage(fileinput);
+    image.drawTo(canvas);
 }
 
-// draw grayscale image on canvas1
+// change image to grayscale
 function makeGray(){
-    image = imageDefault;
-    for(var pixel of image.values()){
+    grayImage = image;
+    for(var pixel of grayImage.values()){
         var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
         pixel.setRed(avg);
         pixel.setGreen(avg);
         pixel.setBlue(avg);
     }
-    var imgcanvas = document.getElementById("can");
-    image.drawTo(imgcanvas);
 }
+
+// draw grayscale image
+function doGray() {
+    if ( imageIsLoaded() ) {     // check if image is loaded
+        makeGray();	                      // function performs the grayscale work
+        grayImage.drawTo(canvas);	          // display image
+    }else{
+        alert("Image not loaded")
+    }
+  }
+
+  // check if there is an image uploaded
+  function imageIsLoaded(){
+    if(image != null){
+        return true;
+    }else{
+        return false;
+    }
+  }
