be2cdb57547d4827058634aa81d46b267ef9c273
danielvillam <jvillame@unal.edu.co>
2023-05-11 12:15:21 -0500

    Add upload and makeGray in code.js to Filters Fun

--- Filters Fun/js/code.js
+++ Filters Fun/js/code.js
@@ -1,8 +1,23 @@
+var image;
+var imageDefault;
+
 // upload image to canvas1
 function upload(){
-    alert("Loaded image")
+    var imgcanvas = document.getElementById("can");
+    var fileinput = document.getElementById("finput");
+    imageDefault = new SimpleImage(fileinput);
+    imageDefault.drawTo(imgcanvas);
 }
 
+// draw grayscale image on canvas1
 function makeGray(){
-    alert("Filter applied")
+    image = imageDefault;
+    for(var pixel of image.values()){
+        var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgcanvas = document.getElementById("can");
+    image.drawTo(imgcanvas);
 }
